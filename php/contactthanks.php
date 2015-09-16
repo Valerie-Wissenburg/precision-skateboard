8c860a43780bc51febb576f9e8c256c18ffcecdd
wobbiejames <robert.jamestaylor@live.com>
2015-09-16 15:16:24 -0500

    contact form
    it works now!!!!!!!!!!!!

---
+++ php/contactthanks.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Contact Page</title>
+<link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="../css/foundation.css" />
+<link rel="stylesheet" href="../css/main.css" />
+<link rel="stylesheet" href="../css/foundation-icons.css" type="text/css"/>
+<script src="../js/vendor/modernizr.js"></script>
+</head>
+
+<body>
+<div class="nav">
+  <div class="columns">
+    <nav class="top-bar" data-topbar>
+      <ul class="title-area">
+        <li class="name"> <a href="../index.html"> <img src="../img/header-logo.png" id="topbar-logo" class="show-for-large-up" alt="logo" width="255" > <img src="../img/sml-logo.png" id="small-topbar-img" class="hide-for-large-up" alt="small logo"/> </a> </li>
+        <li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
+      </ul>
+      <section class="top-bar-section">
+        <ul class="right">
+          <li class="divider"></li>
+          <li><a href="../index.html">Home</a></li>
+          <li class="divider"></li>
+          <li><a href="../history.html">History</a></li>
+          <li class="divider"></li>
+          <li><a href="../team-page.html">Team</a></li>
+          <li class="divider"></li>
+          <li><a href="../contact.html">Contact</a></li>
+          <li class="divider"></li>
+          <li><a href="http://www.precisionskateboard.com/" target="_blank">Blog</a></li>
+        </ul>
+      </section>
+    </nav>
+  </div>
+</div>
+			
+			
+		<br /><br /><br />
+        <br /><br /><br />
+        <br /><br /><br />
+        <br /><br /><br />
+		
+			
+		<h1>Your message has been sent!</h1><br />
+        
+        <br /><br /><br />
+        <br /><br /><br />
+		
+		<h2><a href="../contact.html">Back to Contact Form</a><h2>
+	
+	</div>
+    
+    
+    
+	
+	<footer class="footer">
+  <div class="row">
+    <div class="small-12 columns">
+      <p class="slogan">Precision Skateboards</p>
+      <nav>
+        <ul class="inline-list">
+          <li class="hover-footer"><a href="https://www.facebook.com/pages/Precision-Skateboards/462603100550?fref=ts"><i class="fi-social-facebook"></i></a></li>
+          <li class="hover-footer"><a href="https://twitter.com/SkatePrecision"><i class="fi-social-twitter"></i></a></li>
+          <li class="hover-footer"><a href="https://instagram.com/precisionskateboards/"><i class="fi-social-instagram"></i></a></li>
+        </ul>
+      </nav>
+      <p class="links"> <a href="../index.html">Home</a> <a href="../history.html">History</a> <a href="../team-page.html">Team</a> <a href="../contact.html">Contact</a> <a href="http://www.precisionskateboard.com/">Blog</a> </p>
+      <p class="copywrite">© Precision Skateboards 2015</p>
+    </div>
+  </div>
+</footer>
+<script src="../js/vendor/jquery.js"></script> 
+<script src="../js/foundation.min.js"></script> 
+<script src="../js/foundation/foundation.topbar.js"></script> 
+<script src="../js/foundation/foundation.abide.js"></script> 
+<script type="text/javascript" src="../https://maps.googleapis.com/maps/api/js?key=AIzaSyDzKC_Mhyh6vRgQfDDGnZMyfufQopivILU&sensor=false"></script> 
+<script src="../js/vendor/map.js"></script> 
+<script>
+      $(document).foundation();
+    </script>
+</body>
+</html>
